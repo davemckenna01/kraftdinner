4aaccf7ff58bd159fac8693842a4bc1052d93996
Dave McKenna <davemckenna01@gmail.com>
2012-02-17 15:39:34 -0500

    added an install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+echo "Installing... "
+
+#copy els.sh and put it in /usr/local/bin
+sudo cp els.sh /usr/local/bin/els.sh
+
+#add alias to user's .bash_profile file for conveniance
+echo alias els=\". els.sh\" >> ~/.bash_profile
+
+echo "Done."
+
+echo "You're all set up."
+echo "But note that els won't be runnable in already open terminals."
+echo "To use, Type \"els\" to navigate around the filesystem better. It's like \"ls\" but easier."
